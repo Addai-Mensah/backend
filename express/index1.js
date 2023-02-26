4dcbdfda7c5681a6929aa234bc53b6f74b09ea6d
Addai-Mensah <mensahaddaai@gmail.com>
2023-02-26 22:53:58 +0000

    delete

--- express/index1.js
+++ express/index1.js
@@ -1,3 +1,4 @@
+const Joi = require ("joi")
 const { json } = require("express");
 const express = require ("express");
 const app = express();
@@ -6,7 +7,9 @@ app.use(express.json())
 const courses = [
     {id:1, name: "course1"},
     {id:2, name: "course2"},
-    {id:3, name: "course3"}
+    {id:3, name: "course3"},
+    {id:4, name: "course4"},
+
 ]
 
 app.get("/", (req,res) =>{
@@ -14,7 +17,7 @@ app.get("/", (req,res) =>{
 })
 
 app.get("/api/courses",(req,res)=>{
-    res.send([1,2,3]);
+    res.send(courses)
 })
 
 
@@ -22,19 +25,78 @@ app.get("/api/courses/:id", (req, res) =>{
     const course = courses.find(c => c.id === parseInt(req.params.id))
     if(!course) res.status(404).send("The course with the given ID was not found.")
     res.send(course);
-})
+}) 
 
 
 app.post("/api/courses/",(req,res) =>{
-    // const course = courses.find(c => c.id === parseInt(req.params.id))
-    // if (!course) res.status(404).semd("This course with given ID cannot be found")
-    const course = {
-        id: courses.lenght +1,
-        name: req.body.name
+
+    const  {error}  = validateCourse(req.body)
+    if (error ){ res.status(400).send(error.details[0].message)
+    return;
     }
 
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+
 })
+
+app.put("/api/courses/:id", (req,res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+     if(!course) return
+     res.status(404).send("The course with the given ID was not found.")
+    
+
+    const  {error}  = validateCourse(req.body)
+    if (error) return res.status(400).send(error.details[0].message);
+    course.name = req.body.name
+    res.send(course)
+
+})
+
+app.delete("/api/courses/:id", (req,res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send("The course with the given ID was not found.")
+    res.send(course);
+
+    const index = courses.indexOf(course)
+    courses.splice(index,1)
+    res.send(course)
+})
+
+
+
+
 app.listen(3000,() => console.log(`Listening on port 3000...`))
 // const k  = process.env.PORT || 3000;
 
 
+ let validateCourse = (course) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course.body,schema);
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// g
+ https:github.com/Addai-Mensah/backend.git
