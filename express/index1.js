0b550d88a2c68f0c8ffd657dff164f74325f4b5d
Addai-Mensah <mensahaddaai@gmail.com>
2023-02-26 23:04:17 +0000

    refactored code

--- express/index1.js
+++ express/index1.js
@@ -60,12 +60,13 @@ app.put("/api/courses/:id", (req,res) =>{
 
 app.delete("/api/courses/:id", (req,res) =>{
     const course = courses.find(c => c.id === parseInt(req.params.id))
-    if(!course) res.status(404).send("The course with the given ID was not found.")
+    if(!course) return
+    res.status(404).send("The course with the given ID was not found.")
     res.send(course);
 
     const index = courses.indexOf(course)
     courses.splice(index,1)
-    res.send(courses)
+    res.send(course)
 })
 
 
